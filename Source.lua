de41fa121855441a048037f5c3743b2d5c9dfde9
Pyseph <45090858+PysephWasntAvailable@users.noreply.github.com>
2022-03-21 11:16:11 +0100

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,4 +1,4 @@
--- Created by Pyseph#1015
+-- Created by Pyseph#0001
 
 local SpecialCharacters = {['\a'] = '\\a', ['\b'] = '\\b', ['\f'] = '\\f', ['\n'] = '\\n', ['\r'] = '\\r', ['\t'] = '\\t', ['\v'] = '\\v', ['\0'] = '\\0'}
 local Keywords = { ['and'] = true, ['break'] = true, ['do'] = true, ['else'] = true, ['elseif'] = true, ['end'] = true, ['false'] = true, ['for'] = true, ['function'] = true, ['if'] = true, ['in'] = true, ['local'] = true, ['nil'] = true, ['not'] = true, ['or'] = true, ['repeat'] = true, ['return'] = true, ['then'] = true, ['true'] = true, ['until'] = true, ['while'] = true, ['continue'] = true}
